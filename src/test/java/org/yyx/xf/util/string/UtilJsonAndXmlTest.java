397be2927fb3240b9836f97f91e8a50f9ac4eb0e
叶云轩.MacBookPro <yzw562638362@icloud.com>
2020-02-05 19:37:15 +0800

    修改测试用例

--- src/test/java/org/yyx/xf/util/string/UtilJsonAndXmlTest.java
+++ src/test/java/org/yyx/xf/util/string/UtilJsonAndXmlTest.java
@@ -17,12 +17,60 @@ public class UtilJsonAndXmlTest {
 
     @Test
     public void xmlToJson() {
-        String xml = "<note>\n" +
-                "<to>George</to>\n" +
-                "<from>John</from>\n" +
-                "<heading>Reminder</heading>\n" +
-                "<body>Don't forget the meeting!</body>\n" +
-                "</note>";
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<alert>\n" +
+                "    <identifier>41000041600000_20200205105119</identifier>\n" +
+                "    <sender>河南省气象局</sender>\n" +
+                "    <senderCode>41000041600000</senderCode>\n" +
+                "    <sendTime>2020-02-05 10:45:00+08:00</sendTime>\n" +
+                "    <status>Actual</status>\n" +
+                "    <msgType>Update</msgType>\n" +
+                "    <scope>Public</scope>\n" +
+                "    <code>\n" +
+                "        <method>\n" +
+                "            <methodName>SPEAKER</methodName>\n" +
+                "            <message>河南省气象局2020年02月05日10时53分39秒大风蓝色预警[IV级/一般]全省大部分地区河南省气象台2020年02月05日10时45分继续发布大风蓝色预警：预计未来24小时，全省大部分地区将出现5级左右偏北风，阵风6到7级。请注意防范。</message>\n" +
+                "            <audienceGrp>31aec396-ba79-46fe-9958-a0787dcfd030,</audienceGrp>\n" +
+                "            <audenceprt>,</audenceprt>\n" +
+                "        </method>\n" +
+                "        <method>\n" +
+                "            <methodName>SMS</methodName>\n" +
+                "            <message>河南省气象台2020年02月05日10时45分继续发布大风蓝色预警：预计未来24小时，全省大部分地区将出现5级左右偏北风，阵风6到7级。请注意防范。</message>\n" +
+                "            <audienceGrp>d90a8856-1183-47b0-8d8d-29b0dbee6216,5080d87d-fd9e-4016-8d8d-2a86df9ab50a,</audienceGrp>\n" +
+                "            <audenceprt>,</audenceprt>\n" +
+                "        </method>\n" +
+                "        <method>\n" +
+                "            <methodName>WEB</methodName>\n" +
+                "            <message>河南省气象局2020年02月05日10时53分发布大风蓝色预警[IV级/一般]河南省气象台2020年02月05日10时45分继续发布大风蓝色预警：预计未来24小时，全省大部分地区将出现5级左右偏北风，阵风6到7级。请注意防范。</message>\n" +
+                "            <audienceGrp/>\n" +
+                "            <audenceprt>,</audenceprt>\n" +
+                "        </method>\n" +
+                "    </code>\n" +
+                "    <secClassification>None</secClassification>\n" +
+                "    <note/>\n" +
+                "    <references>41000041600000_20200204121842</references>\n" +
+                "    <info>\n" +
+                "        <language>zh-CN</language>\n" +
+                "        <eventType>11B06</eventType>\n" +
+                "        <urgency>Unknown</urgency>\n" +
+                "        <severity>Blue</severity>\n" +
+                "        <certainty>Unknown</certainty>\n" +
+                "        <effective>2020-02-05 10:57:42+08:00</effective>\n" +
+                "        <onset/>\n" +
+                "        <expires/>\n" +
+                "        <senderName>李飞</senderName>\n" +
+                "        <headline>河南省气象局发布大风蓝色预警[IV级/一般]</headline>\n" +
+                "        <description>河南省气象台2020年02月05日10时45分继续发布大风蓝色预警：预计未来24小时，全省大部分地区将出现5级左右偏北风，阵风6到7级。请注意防范。</description>\n" +
+                "        <instruction/>\n" +
+                "        <web/>\n" +
+                "        <area>\n" +
+                "            <areaDesc>河南省,河南省</areaDesc>\n" +
+                "            <polygon/>\n" +
+                "            <circle/>\n" +
+                "            <geocode>410000000000,410000000000</geocode>\n" +
+                "        </area>\n" +
+                "    </info>\n" +
+                "</alert>\n";
         try {
             String s = UtilJsonAndXml.xmlToJson(xml);
             LOGGER.info("[json] {}", s);
