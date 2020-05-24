ce0b9f78adec6e565b39a4290a56e073a992f8fc
叶云轩 <marquis_xuan@163.com>
2020-05-24 21:44:33 +0800

    删除主界面

---
+++ src/main/java/org/yyx/xf/tool/web/util/UtilRequest.java
@@ -0,0 +1,18 @@
+package org.yyx.xf.tool.web.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 请求工具类
+ * <p>
+ *
+ * @author 叶云轩 at tdg_yyx@foxmail.com
+ * @date 2020/5/11 3:35 下午
+ */
+public class UtilRequest {
+
+
+    public static String getDataByPostRequest(HttpServletRequest httpServletRequest){
+
+    }
+}
